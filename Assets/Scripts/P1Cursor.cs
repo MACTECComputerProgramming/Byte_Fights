19a213bf39f962d92ccedb87031aad1715d6b97c
Iceflame21 <19cmsnow@gmail.com>
2018-10-21 21:16:15 -0500

    Polishing Some Areas for Demo (Colby)

---
+++ Assets/Scripts/P1Cursor.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1Curser : MonoBehaviour {
+    public string buttonName;
+    private Transform thisTransform;
+    private Rigidbody2D theRB;
+    public SpriteRenderer Player1;
+	// Use this for initialization
+	void Start () {
+        theRB = GetComponent<Rigidbody2D>();
+        Player1 = GetComponent<SpriteRenderer>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        Movement();
+    }
+        void Movement()
+        {
+        float movespeed = 1;
+        movespeed = movespeed * Input.GetAxis("LeftJoystickHorizontal");
+        if (Input.GetAxis("LeftJoystickHorizontal") == 1)
+        {
+          
+            theRB.velocity = new Vector2(movespeed, theRB.velocity.y);
+        }
+        else if (Input.GetAxis("LeftJoystickHorizontal") == -1)
+        {
+            
+            theRB.velocity = new Vector2(movespeed, theRB.velocity.y);
+        }
+        else
+        {
+
+            theRB.velocity = new Vector2(0, theRB.velocity.y);
+        }
+
+      
+        movespeed = movespeed * Input.GetAxis("LeftJoystickVertical");
+        if (Input.GetAxis("LeftJoystickVertical") == 1)
+        {
+       
+            theRB.velocity = new Vector2(theRB.velocity.y,movespeed );
+        }
+        else if (Input.GetAxis("LeftJoystickVertical") == -1)
+        {
+           
+            theRB.velocity = new Vector2(theRB.velocity.y,movespeed);
+        }
+        else
+        {
+
+            theRB.velocity = new Vector2(0, theRB.velocity.y);
+        }
+        }
+	}
+
---
+++ Assets/Scripts/P2Cursor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P2Cursor : MonoBehaviour {
+    public string buttonName;
+    private Transform thisTransform;
+    private Rigidbody2D theRB;
+    public SpriteRenderer Player1;
+    // Use this for initialization
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody2D>();
+        Player1 = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        float movespeed = 1;
+        movespeed = movespeed * Input.GetAxis("p2LeftJoystickHorizontal");
+        if (Input.GetAxis("p2LeftJoystickHorizontal") == 1)
+        {
+
+            theRB.velocity = new Vector2(movespeed, theRB.velocity.y);
+        }
+        else if (Input.GetAxis("p2LeftJoystickHorizontal") == -1)
+        {
+
+            theRB.velocity = new Vector2(movespeed, theRB.velocity.y);
+        }
+        else
+        {
+
+            theRB.velocity = new Vector2(0, theRB.velocity.y);
+        }
+
+
+        movespeed = movespeed * Input.GetAxis("p2LeftJoystickVertical");
+        if (Input.GetAxis("p2LeftJoystickVertical") == 1)
+        {
+
+            theRB.velocity = new Vector2(theRB.velocity.y, movespeed);
+        }
+        else if (Input.GetAxis("p2LeftJoystickVertical") == -1)
+        {
+
+            theRB.velocity = new Vector2(theRB.velocity.y, movespeed);
+        }
+        else
+        {
+
+            theRB.velocity = new Vector2(0, theRB.velocity.y);
+        }
+    }
+}
