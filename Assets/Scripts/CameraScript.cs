568fbbc879519ea10b495474b820ebde4fe95c9f
Iceflame21 <19cmsnow@macon.k12.mo.us>
2018-10-24 13:45:32 -0500

    Revert "Merge branch 'master' of https://github.com/MACTECComputerProgramming/Byte_Fights"
    This reverts commit fd17b17d44eef489a81c74c61d245c412f11d31d, reversing
    changes made to a1e8154fcbd61dc0f00b2280b7367ac788313c34.

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -6,6 +6,13 @@ public class CameraScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-   
+        float width = Screen.width;
+        float height = Screen.height;
+        float ar = Mathf.RoundToInt((width / height) * 100f) / 100f;
+        if (ar == 0.75f)
+        {
+            ar = 0.65f;
+        }
+        gameObject.GetComponent<Camera>().aspect = ar;
 	}
 }
