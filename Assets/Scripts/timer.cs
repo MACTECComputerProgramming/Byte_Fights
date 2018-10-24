c7786e6d8e16fc03b6fe80f6c5f753142a3ab7f5
Camden <19hcsmith@macon.k12.mo.us>
2018-10-24 14:06:43 -0500

    fight timer finished camden

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class timer : MonoBehaviour {
+    Text text;
+    public static float timeLeft = 60f;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 1)
+
+            timeLeft = 1;
+        text.text = " " + Mathf.Round(timeLeft);
+
+        if (timeLeft == 1)
+            SceneManager.LoadScene("Main");
+
+	
+	}
+}
