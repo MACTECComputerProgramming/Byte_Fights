d80089a790b05736d987ce2dae84f7618700dabc
Derphitene <43854699+Derphitene@users.noreply.github.com>
2018-10-30 14:17:32 -0500

    Winner part of fight started

--- Assets/Scripts/Winner.cs
+++ Assets/Scripts/Winner.cs
@@ -2,20 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor.SceneManagement;
+using UnityEngine.UI;
 
 public class Winner : MonoBehaviour {
-
+    Text text;
 	// Use this for initialization
 	void Start () {
-		
+
+        text = GetComponent<Text>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-       if( PlayerScript.health == 0)
-       {
 
+        if (HealthScript.Player2Hp.fillAmount == 0)
+       {
+           text.text = "Player 1 Wins ";
        }
 
 
