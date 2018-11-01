70e0c00d0b2e3ca8a4cad5df238a7cf96beb40ec
Derphitene <43854699+Derphitene@users.noreply.github.com>
2018-11-01 13:15:25 -0500

    winner script

--- Assets/Scripts/Winner.cs
+++ Assets/Scripts/Winner.cs
@@ -1,33 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 
 public class Winner : MonoBehaviour {
     public Text text;
     public Image player2HP;
     public Image player1HP;
-    
+    public double swapscenenumber = 5;
 	// Use this for initialization
 	void Start () {
 
         text = GetComponent<Text>();
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+        
 
         if (player2HP.fillAmount == 0)
        {
            text.text = "Player 1 Wins ";
+           GameObject.Find("Player 2").transform.localScale = new Vector3(0, 0, 0);
+           if (swapscenenumber == 0)
+           {
+               SceneManager.LoadScene("Main");
+           }
+           else
+           {
+               swapscenenumber -= 1;
+           }
        }
+
+
+
+
+
+
+
+
 
         if (player1HP.fillAmount == 0)
         {
             text.text = "Player 2 Wins ";
+            GameObject.Find("Player 2").transform.localScale = new Vector3(0, 0, 0);
+            if (swapscenenumber == 0)
+            {
+                SceneManager.LoadScene("Main");
+            }
+            else
+            {
+                swapscenenumber -= 1;
+            }
+           
         }
 
+       
     }
 }
