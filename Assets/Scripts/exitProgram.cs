8c76fce0ca2510b97f40f2904cf38234ba6d55c4
Zoe <20zlisaacson@macon.k12.mo.us>
2018-11-06 13:00:35 -0600

    comments

--- Assets/Scripts/exitProgram.cs
+++ Assets/Scripts/exitProgram.cs
@@ -3,37 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class exitProgram : MonoBehaviour {
+    //instantiates game objects- cursors for player one and player two
     private GameObject player1;
     private GameObject player2;
 
     // Use this for initialization
     void Start()
     {
+        //assigns the cursors in the game to the game objects instantiated above
         player1 = GameObject.Find("P1Cursor");
         player2 = GameObject.Find("P2Cursor");
     }
 
+    //Checks if the box collider is triggered and stays in code until it is no longer true
     private void OnTriggerStay2D(Collider2D coll)
-    {
+    {   //Log placed to check if code is working
         Debug.Log("Entered");
+        //Checks if player one's a button is pressed
         if (Input.GetButton("P1AButton"))
         {
+            //Log placed to check if code is working
             Debug.Log("A Button");
+            //Checks if the game object colliding with the button is player one's cursor
             if (coll.gameObject == player1)
             {
+                //calls method to exit game
                 QuitGame();
             }
         }
+        //Checks if player two's a button is pressed
         if (Input.GetButton("P2AButton"))
-        {
+        {   //Checks if the game object colliding with the button is player two's cursor
             if (coll.gameObject == player2)
-            {
+            {   
+                //calls method to exit scene
                 QuitGame();
             }
         }
     }
+    //method to exit program
     public void QuitGame()
     {
+        //exits program
         Application.Quit();
         Debug.Log("Game is exiting");
         //just to check if it's working.
--- Assets/Scripts/loadScene.cs
+++ Assets/Scripts/loadScene.cs
@@ -5,12 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class loadScene : MonoBehaviour
 {
+    //instantiates game objects - cursors for player one and player two
     private GameObject player1;
     private GameObject player2;
 
     // Use this for initialization
     void Start()
-    {
+    { // finds the cursors in the game and assigns them to the game objects
         player1 = GameObject.Find("P1Cursor");
         player2 = GameObject.Find("P2Cursor");
     }
@@ -21,21 +22,31 @@ public class loadScene : MonoBehaviour
 
     }
 
+    //detects when the box collider is triggered
     private void OnTriggerStay2D(Collider2D coll)
     {
+        //log allows us to make sure the game is working
         Debug.Log("Entered");
+
+        //Checks if Player One's A button is pressed.
         if (Input.GetButton("P1AButton"))
         {
+            //Log placed to ensure the script was working
             Debug.Log("A Button");
+            //Checks if the object colliding with the button is player one's cursor
             if (coll.gameObject == player1)
             {
+                //loads the Character Select scene
                 SceneManager.LoadScene("Character_Select");
             }
         }
+        //Checks if Player Two's A button is pressed
         if (Input.GetButton("P2AButton"))
         {
+            // Checks if the object colliding with the button is player two's cursor
             if (coll.gameObject == player2)
             {
+                //loads the Character Select scene
                 SceneManager.LoadScene("Character_Select");
             }
         }
